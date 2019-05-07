d99bbeabab5e6a2c5c84308b6427813883971046
rodion <rodionaaafgh@gmail.com>
2019-05-07 13:45:32 +0300

    Updates

---
+++ precache-manifest.866adb3cefb93d066ac696c8a1006526.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2af1b985c11dc1909c310a1800f4b0c0",
+    "url": "/linear-algebra/index.html"
+  },
+  {
+    "revision": "05ceb9e6c167debb1806",
+    "url": "/linear-algebra/static/js/2.a69c78e2.chunk.js"
+  },
+  {
+    "revision": "391fb95693247828a5b5",
+    "url": "/linear-algebra/static/js/main.e0d3c032.chunk.js"
+  },
+  {
+    "revision": "5c943c56494f6c1e2826",
+    "url": "/linear-algebra/static/js/runtime~main.420b36d7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d9c647998eb6d855c4effbbf624c9744.js"
+  "/linear-algebra/precache-manifest.866adb3cefb93d066ac696c8a1006526.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/linear-algebra/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
