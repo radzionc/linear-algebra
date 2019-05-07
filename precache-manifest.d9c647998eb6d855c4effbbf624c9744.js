7dacea64473d4c989e273a70336bc6fce385fff8
rodion <rodionaaafgh@gmail.com>
2019-05-07 12:25:13 +0300

    Updates

---
+++ precache-manifest.d9c647998eb6d855c4effbbf624c9744.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d99a21680657b1fa0267148a5689756",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05654ce680bd128869cf",
+    "url": "/static/js/2.421fbdc0.chunk.js"
+  },
+  {
+    "revision": "6cfbb6d09593f88313af",
+    "url": "/static/js/main.74b827fc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
