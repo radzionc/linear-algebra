97f488592eadefc68d3dbf835b25bc99b16173f0
rodion <rodionaaafgh@gmail.com>
2019-05-07 13:54:43 +0300

    update: ui and readme

--- Readme.md
+++ Readme.md
@@ -1,6 +1,15 @@
 # linear-algebra
 > 
 
+## [Demo](https://rodionchachura.github.io/linear-algebra/)
+
+### Run demo docally
+```bash
+cd linear-algebra-demo
+npm install
+npm start
+```
+
 ![alt text](https://cdn-images-1.medium.com/max/800/1*4yaaTk2eqnmn19nyorh-HA.png)
 
 ## License
--- linear-algebra-demo/src/components/menu-item.js
+++ linear-algebra-demo/src/components/menu-item.js
@@ -3,8 +3,13 @@ import styled from 'styled-components'
 
 const Container = styled.p`
   cursor: pointer;
+  margin: 10px;
   color: ${p =>
     p.selected ? p.theme.color.mainText : p.theme.color.secondaryText};
+  transition: 0.35s ease-in-out;
+  &:hover {
+    color: ${p => p.theme.color.mainText};
+  }
 `
 
 export default ({ text, onClick, selected }) => (
