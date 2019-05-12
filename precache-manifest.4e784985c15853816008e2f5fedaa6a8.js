87f0c1586fe649a5487d3c6fec84c19bec868a4a
rodion <rodionaaafgh@gmail.com>
2019-05-12 16:59:06 +0300

    Updates

---
+++ precache-manifest.4e784985c15853816008e2f5fedaa6a8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a79ad3d454f794a33c40b2d8f96d300",
+    "url": "/linear-algebra/index.html"
+  },
+  {
+    "revision": "11b4b359da8049f199aa",
+    "url": "/linear-algebra/static/js/2.9c800698.chunk.js"
+  },
+  {
+    "revision": "a7725b2a3cc275084013",
+    "url": "/linear-algebra/static/js/main.0ce73385.chunk.js"
+  },
+  {
+    "revision": "5c943c56494f6c1e2826",
+    "url": "/linear-algebra/static/js/runtime~main.420b36d7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/linear-algebra/precache-manifest.0e18add34a9d3659e2e8770b5d6b878d.js"
+  "/linear-algebra/precache-manifest.4e784985c15853816008e2f5fedaa6a8.js"
 );
 
 self.addEventListener('message', (event) => {
